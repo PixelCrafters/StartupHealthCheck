00e200cb18a2581971ca4f2256e9f4c8c0346a61
Jenn Cooper <cooper.jennl@gmail.com>
2014-04-29 15:46:21 -0700

    add StartupGenome::API

---
+++ lib/startup_genome/api.rb
@@ -0,0 +1,34 @@
+require 'faraday'
+
+module StartupGenome
+  class API
+    def initialize
+      @host = 'http://startupgenome.co/api/'
+      @path = 'organizations/city/'
+      @location_slug = Rails.application.config.startup_genome_location_slug
+      @auth_code = Rails.application.secrets.startup_genome_auth_code
+
+      raise 'You must provide a Startup Genome Authorization Code' if @auth_code.nil?
+    end
+
+    # TODO: Pass in organizations path to method rather than setting on obj creation
+    def get_organizations
+      connect
+      @conn.get url, {}, {'auth-code' => @auth_code}
+    end
+
+    private
+
+    def connect
+      @conn = Faraday.new(:url => @host) do |faraday|
+        faraday.request  :url_encoded            
+        faraday.response :logger                 
+        faraday.adapter  Faraday.default_adapter
+      end
+    end
+
+    def url
+      "#{@host}#{@path}#{@location_slug}"
+    end
+  end
+end 
