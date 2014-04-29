2f5568a0754760a411d1c664cc079feeb6e0bca3
Jenn Cooper <cooper.jennl@gmail.com>
2014-04-29 16:30:48 -0700

    change order of vars for consistency

--- lib/startup_genome/api.rb
+++ lib/startup_genome/api.rb
@@ -5,8 +5,8 @@ module StartupGenome
     def initialize
       @host = 'http://startupgenome.co/api/'
       @path = 'organizations/city/'
-      @location_slug = Rails.application.config.startup_genome_location_slug
       @auth_code = Rails.application.secrets.startup_genome_auth_code
+      @location_slug = Rails.application.config.startup_genome_location_slug
 
       raise 'You must provide a Startup Genome Authorization Code' if @auth_code.nil?
       raise 'You must provide a Startup Genome Location Slug' if @location_slug.nil?
