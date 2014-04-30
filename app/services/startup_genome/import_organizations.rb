371bbed47df5dc97aa1c9580b56fab9ddd43ab50
Jenn Cooper <cooper.jennl@gmail.com>
2014-04-30 10:42:39 -0700

    add Service module that extends ActiveSupport::Concern

--- app/services/startup_genome/import_organizations.rb
+++ app/services/startup_genome/import_organizations.rb
@@ -1,26 +1,27 @@
 module StartupGenome
   class ImportOrganizations
-    def self.call
-      sg = StartupGenome::API.new
-      results = sg.get_organizations
+    include Service
+
+    def call
+      results = StartupGenome::API.new.get_organizations
       persist(JSON.parse results.body)
     end
   
     private
 
-    def self.persist(orgs)
+    def persist(orgs)
       orgs.first[1].each do |org|
         create_or_update_organization(org)
       end
     end
 
-    def self.create_or_update_organization(org)
+    def create_or_update_organization(org)
       data = build_data_hash(org)
       organization = Organization.find_or_initialize_by(startup_genome_id: org[1]["organization_id"])
       organization.update(data) ? print(true, org[1]["name"]) : print(false, org[1]["name"])
     end
 
-    def self.build_data_hash(org)
+    def build_data_hash(org)
       { name: org[1]["name"],
         headline: org[1]["headline"],
         description: org[1]["description"],
@@ -33,7 +34,7 @@ module StartupGenome
         startup_genome_id: org[1]["organization_id"] }
     end
 
-    def self.print(success, org_name)
+    def print(success, org_name)
       puts success ? "Successfully created/updated #{org_name}" : "Failed to update #{org_name}"
     end
   end
