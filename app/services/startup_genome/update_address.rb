709bd7c98167f1bb90b17a3f2382b96e4fbf6e93
Jenn Cooper <cooper.jennl@gmail.com>
2014-05-05 09:03:33 -0700

    refactor update_organization service

---
+++ app/services/startup_genome/update_address.rb
@@ -0,0 +1,35 @@
+module StartupGenome
+  class UpdateAddress
+    include Service
+
+    attr_reader :org_hash, :organization
+
+    def initialize(org_hash, organization)
+      @org_hash = org_hash
+      @organization = organization
+    end
+
+    def call
+      update_address(org_hash, organization)
+    end
+
+    private
+
+    def update_address(org_hash, organization)
+      data = build_address_hash(org_hash)
+      address = Address.find_or_initialize_by(organization_id: organization.id)
+      msg = address.update!(data) ? "Successfully created/updated address of #{org_hash['name']}" : "Failed to update address of #{org_hash['name']}"
+      puts msg
+    end
+
+    def build_address_hash(org_hash)
+      { latitude: org_hash["latitude"],
+        longitude: org_hash["longitude"],
+        zip: org_hash["zip"],
+        address1: org_hash["address1"], 
+        address2: org_hash["address2"],
+        state: org_hash["state"],
+        country: org_hash["country"] }
+    end
+  end
+end
--- app/services/startup_genome/update_organization.rb
+++ app/services/startup_genome/update_organization.rb
@@ -23,12 +23,8 @@ module StartupGenome
       organization
     end
 
-    # TODO: Extract this out into it's own service
     def update_address(org_hash, organization)
-      data = build_address_hash(org_hash)
-      address = Address.find_or_initialize_by(organization_id: organization.id)
-      msg = address.update!(data) ? "Successfully created/updated address of #{org_hash['name']}" : "Failed to update address of #{org_hash['name']}"
-      puts msg
+      StartupGenome::UpdateAddress.call(org_hash, organization)
     end
 
     def build_data_hash(org_hash)
@@ -42,16 +38,6 @@ module StartupGenome
         url: org_hash["url"],
         founded: org_hash["founded"].to_i,
         startup_genome_id: org_hash["organization_id"] }
-    end
-
-    def build_address_hash(org_hash)
-      { latitude: org_hash["latitude"],
-        longitude: org_hash["longitude"],
-        zip: org_hash["zip"],
-        address1: org_hash["address1"], 
-        address2: org_hash["address2"],
-        state: org_hash["state"],
-        country: org_hash["country"] }
     end
   end
 end
