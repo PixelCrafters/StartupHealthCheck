8ed21581846eef5b2b9ebd5b97b280caa97e1c47
Jenn Cooper <cooper.jennl@gmail.com>
2014-05-05 13:43:18 -0700

    extract ORG_HASH into support file

--- spec/services/startup_genome/import_organizations_spec.rb
+++ spec/services/startup_genome/import_organizations_spec.rb
@@ -1,16 +1,12 @@
 require 'spec_helper'
 
 describe StartupGenome::ImportOrganizations do
-
   describe "#call" do
-
     it "imports" do
       VCR.use_cassette 'startup_genome_api_get_organizations' do
         StartupGenome::UpdateOrganization.should_receive(:call).with(any_args()).exactly(429).times
         StartupGenome::ImportOrganizations.call
       end
     end
-
   end
-
 end
