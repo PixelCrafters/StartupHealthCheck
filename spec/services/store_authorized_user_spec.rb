d041cc47fac876e9140ceee55bf57683fc38ae29
Jenn Cooper <cooper.jennl@gmail.com>
2014-05-07 10:26:17 -0700

    add update specs for StoreAuthorizedUser

--- spec/services/store_authorized_user_spec.rb
+++ spec/services/store_authorized_user_spec.rb
@@ -6,5 +6,25 @@ describe StoreAuthorizedUser do
       user = StoreAuthorizedUser.call(USER_INFO_HASH_TWITTER)
       expect(user.persisted?).to eq true
     end
+
+    it "should update the user" do
+      user = StoreAuthorizedUser.call(USER_INFO_HASH_TWITTER)
+
+      hash_dupe = USER_INFO_HASH_TWITTER.dup
+      hash_dupe["info"]["name"] = "whatever"
+
+      user = StoreAuthorizedUser.call(hash_dupe)
+      expect(user.name).to eq "whatever"
+    end
+
+    it "should not replace data with nil" do
+      user = StoreAuthorizedUser.call(USER_INFO_HASH_TWITTER)
+
+      hash_dupe = USER_INFO_HASH_TWITTER.dup
+      hash_dupe["info"]["name"] = nil
+
+      user = StoreAuthorizedUser.call(hash_dupe)
+      expect(user.name).to_not be_nil
+    end
   end
 end
