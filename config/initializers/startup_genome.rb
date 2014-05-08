bcdea8b1fa70ccac868dfdf52dab7d5ce9b7d8e3
Jenn Cooper <cooper.jennl@gmail.com>
2014-05-08 10:03:06 -0700

    remove custom config from initializer

--- config/initializers/startup_genome.rb
+++ config/initializers/startup_genome.rb
@@ -1,4 +1,2 @@
-STARTUP_GENOME_CONFIG = YAML.load_file("#{::Rails.root}/config/startup_genome.yml")[::Rails.env]
-
 # Location slug from Startup Genome
 Rails.application.config.startup_genome_location_slug = "vancouver-britishcolumbia-can"
