ff336acd2106f58e98bdea0cfa6c9347e139613a
Jenn Cooper <cooper.jennl@gmail.com>
2014-05-20 17:11:32 -0700

    only index

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
   get 'tags/:tag', to: 'organizations#index', as: :tag
   delete 'tag/:organization_id/:tag_id', to: 'organizations#destroy_tag', as: "destroy_tag"
 
-  resources :activities
+  resources :activities, only: [:index]
 end
