eb5c18bd1a51a9283c1e3237f9d2302d6ad1c090
Jenn Cooper <cooper.jennl@gmail.com>
2014-05-26 16:51:33 -0700

    order steps by type

--- features/step_definitions/organizations/roles_steps.rb
+++ features/step_definitions/organizations/roles_steps.rb
@@ -2,10 +2,6 @@ Given(/^a role named "(.*?)"$/) do |role_name|
   Role.create!(name: role_name)
 end
 
-Then(/^I should see my role "(.*?)"$/) do |role_name|
-  expect(page).to have_content(role_name)
-end
-
 When(/^I add my role "(.*?)"$/) do |role_name|
   select "Employee", from: "role_id"
   click_button "Submit"
@@ -14,4 +10,8 @@ end
 When(/^I edit my profile page$/) do
   visit user_path(@organization.users.first)
   click_link "edit"
+end
+
+Then(/^I should see my role "(.*?)"$/) do |role_name|
+  expect(page).to have_content(role_name)
 end
