3f130b11b4a3e0f3f37b0b6bd5994d92d35d056c
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-10 14:28:44 -0700

    add style to activities page

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Startuphealthcheck
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(#{config.root}/lib)
+
+    config.assets.paths << Rails.root.join("app", "assets", "icons")
   end
 end
