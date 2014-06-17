9bf8e6dbee7aef726692629752477f0017d1dc85
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-17 14:45:27 -0700

    add filter on activities passed in email

--- app/mailers/activity_mailer.rb
+++ app/mailers/activity_mailer.rb
@@ -1,7 +1,13 @@
 class ActivityMailer < ActionMailer::Base 
   def weekly_digest(user)
     @user = user
-    @activities = PublicActivity::Activity.last(20).reverse
+    @activities = filtered_activities
     mail(to: @user.email, subject: "Your Email Digest for #{l Date.today, format: :long}", from: "digest@weareyvr.ca")
+  end
+
+  private
+
+  def filtered_activities
+    PublicActivity::Activity.combined_distinct.take(20).reverse
   end
 end
