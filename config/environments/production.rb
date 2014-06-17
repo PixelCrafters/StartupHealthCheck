5cce0b9fc9b69c42968136d1aef25e8069928293
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-17 15:16:46 -0700

    add host only_path false and move mandrill env vars to secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,16 +81,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = {
+    host: "weareyvr.herokuapp.com",
+    only_path: false
+  }
   config.action_mailer.smtp_settings = {
     address:   "smtp.mandrillapp.com",
     port:      587,
     enable_starttls_auto: true,
-    user_name: ENV['MANDRILL_USERNAME'],
-    password:  ENV['MANDRILL_PASSWORD'],
+    user_name: Rails.application.secrets.mandrill_username,
+    password:  Rails.application.secrets.mandrill_password,
     authentication: 'login',
     domain: 'weareyvr.herokuapp.com'
   }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "weareyvr.herokuapp.com" }
-
 end
