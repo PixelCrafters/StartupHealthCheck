c40ba5fb0575d801d0475a795f11a9137069684e
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-19 09:26:21 -0700

    prevent users from claiming multiple profiles

--- features/step_definitions/users/claim_steps.rb
+++ features/step_definitions/users/claim_steps.rb
@@ -1,15 +1,16 @@
 Given(/^there is a user available$/) do
-  @user = FactoryGirl.create(:user)
-end
-
-When(/^I visit the user profile page$/) do
-  visit user_path(@user)
+  @user1 = FactoryGirl.create(:user, name: "Example User")
 end
 
 When(/^I claim the user profile$/) do
   click_link "click here"
 end
 
+When(/^I visit the user profile page for "(.*?)"$/) do |user_name|
+  user = User.where(name: user_name).first
+  visit user_path(user)
+end
+
 Then(/^I should own the profile$/) do
-  expect(UserAuthService.first.user).to eq(@user)
+  expect(UserAuthService.first.user).to eq(@user1)
 end
--- features/users/claim.feature
+++ features/users/claim.feature
@@ -8,7 +8,7 @@ Feature: Claim user profile
     Given there is a user available
 
   Scenario: Visitor claims profile
-    When I visit the user profile page
+    When I visit the user profile page for "Example User"
     And I claim the user profile
     And I login with "Twitter"
     And I submit an email address
