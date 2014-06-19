d37f8795947f3e17cf2d15b4a2a1f144256d5a5d
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-19 13:57:48 -0700

    update create and destroy services to include user tags

--- app/services/tag/create.rb
+++ app/services/tag/create.rb
@@ -3,17 +3,28 @@ class Tag::Create
 
   attr_reader :tag_name, :organization, :current_user
 
-  def initialize(tag_name, organization, current_user)
+  def initialize(tag_name, organization=nil, current_user)
     @tag_name = tag_name
     @organization = organization
     @current_user = current_user
   end
 
   def call
-    if organization.tag_list.add(tag_name)
-      organization.save
-      organization.create_activity key: "tag.add", owner: current_user, parameters: {tag_name: tag_name}
-      organization.tag_list.last
+    if organization.nil?
+      create_tag(current_user)
+    else
+      create_tag(organization)
+    end
+  end
+
+  private
+
+  def create_tag(obj)
+    owner = obj.is_a?(Organization) ? current_user : nil
+    if obj.tag_list.add(tag_name)
+      obj.save
+      obj.create_activity key: "tag.add", owner: owner, parameters: {tag_name: tag_name}
+      obj.tag_list.last
     else
       nil
     end
--- app/services/tag/destroy.rb
+++ app/services/tag/destroy.rb
@@ -3,17 +3,28 @@ class Tag::Destroy
 
   attr_reader :tag_id, :organization, :current_user
 
-  def initialize(tag_id, organization, current_user)
+  def initialize(tag_id, organization=nil, current_user)
     @tag_id = tag_id
     @organization = organization
     @current_user = current_user
   end
 
   def call
-    tag = organization.tags.find(tag_id)
-    if organization.tag_list.remove(tag.name)
-      organization.save
-      organization.create_activity key: "tag.remove", owner: current_user, parameters: {tag_name: tag.name}
+    if organization.nil?
+      destroy_tag(current_user)
+    else
+      destroy_tag(organization)
+    end
+  end
+
+  private
+
+  def destroy_tag(obj)
+    owner = obj.is_a?(Organization) ? current_user : nil
+    tag = obj.tags.find(tag_id)
+    if obj.tag_list.remove(tag.name)
+      obj.save
+      obj.create_activity key: "tag.remove", owner: owner, parameters: {tag_name: tag.name}
       tag
     else
       nil
