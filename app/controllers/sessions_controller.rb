2cd7f5e1f1a3141bee42409414f2a0c36e5c5c1a
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-19 16:26:35 -0700

    reset session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,8 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     session[:userinfo] = nil
     session[:current_connection] = nil
+    session[:claimed_user_id] = nil
+    reset_session
     redirect_to root_path
   end
 
