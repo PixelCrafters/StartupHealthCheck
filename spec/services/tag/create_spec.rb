1d895382f9133953c65658bcf127e1269dc84970
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-19 13:50:26 -0700

    adjust tag creation spec to test for user tags

--- spec/services/tag/create_spec.rb
+++ spec/services/tag/create_spec.rb
@@ -6,9 +6,20 @@ describe Tag::Create do
     organization = FactoryGirl.build(:organization)
     tag_name = "startup"
 
-    it "should add the tag to the organization's tag list" do
-      result = Tag::Create.call(tag_name, organization, current_user)
-      expect(result).to eq tag_name
+    context "organization exists" do
+      it "should return the tag name after adding the tag to the organization's tag list" do
+        result = Tag::Create.call(tag_name, organization, current_user)
+        expect(result).to eq tag_name
+        expect(organization.tags.last.name).to eq tag_name
+      end
+    end
+
+    context "organization is nil" do
+      it "should return the tag name after adding the tag to the user's tag list" do
+        result = Tag::Create.call(tag_name, nil, current_user)
+        expect(result).to eq tag_name
+        expect(current_user.tags.last.name).to eq tag_name
+      end
     end
   end
 end
