650ede7789060a94d419c9b44d810c431bde0ce3
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-23 16:45:28 -0700

    can use secrets here

--- lib/auth0/api.rb
+++ lib/auth0/api.rb
@@ -3,9 +3,9 @@ require 'faraday'
 module Auth0
   class API
     def initialize
-      @host = "https://" + (ENV['AUTH0_DOMAIN'] || Rails.application.secrets.auth0_domain)
-      @auth0_client_id =  ENV['AUTH0_CLIENT_ID'] || Rails.application.secrets.auth0_client_id
-      @auth0_secret =  ENV['AUTH0_SECRET'] ||  Rails.application.secrets.auth0_secret
+      @host = "https://" + Rails.application.secrets.auth0_domain
+      @auth0_client_id = Rails.application.secrets.auth0_client_id
+      @auth0_secret = Rails.application.secrets.auth0_secret
       @conn = APIConnection.connect_via_faraday(@host)
     end
 
