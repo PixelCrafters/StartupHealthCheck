76bf941aec2adb4d37eff7fa2f8b1eaeeddd0616
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-23 10:40:28 -0700

    improve style on organization show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,9 @@ module ApplicationHelper
   def user_is_current_user?
     (current_user.present? && @user == current_user)
   end
+
+  def organization_admin_user?
+    current_user && (current_user == @organization.users.first) 
+  end
 end
   
--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -2,4 +2,19 @@ module OrganizationsHelper
   def status
     @organization.active ? "active" : "not active"
   end
+
+  def display_address
+    address = ""
+    if @organization.main_address.address1
+      address = "#{@organization.main_address.address1}, "
+    end
+    if @organization.main_address.address2
+      address += "#{@organization.main_address.address2}, "
+    else
+      address += "#{Rails.application.secrets.location_name}, "
+    end
+    if @organization.main_address.zip
+      address += "#{@organization.main_address.zip}"
+    end
+  end
 end
