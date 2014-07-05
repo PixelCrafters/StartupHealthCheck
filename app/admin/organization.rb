e43c971d56d2c15009feb7d860764731591e9ebb
Boris Mann <boris@bmannconsulting.com>
2014-07-05 13:29:43 -0700

    Removing filters so that AA organizations list works

--- app/admin/organization.rb
+++ app/admin/organization.rb
@@ -13,5 +13,23 @@ ActiveAdmin.register Organization do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
+  # AA doesn't handle has_many well
+  remove_filter :organization_user_roles
+  remove_filter :addresses
+  remove_filter :profile_links
+  remove_filter :roles
+  remove_filter :users
+  remove_filter :activities
+  remove_filter :taggings
+  remove_filter :base_tags
+  remove_filter :tag_taggings
+  remove_filter :slug
+  remove_filter :startup_genome_slug
+  remove_filter :hiring_url
+  remove_filter :url
+  remove_filter :image
+  remove_filter :organizations_users
+  remove_filter :organizations_types
+
 end
