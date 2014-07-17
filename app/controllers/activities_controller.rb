022f4a8506d83583839b4fe639b74b8b2b182430
Jenn Cooper <cooper.jennl@gmail.com>
2014-07-17 11:02:37 -0700

    fix home page pagination. Fixes #54

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.order('created_at desc').page
+    @activities = PublicActivity::Activity.order('created_at desc').page(params[:page]) 
   end
 end
